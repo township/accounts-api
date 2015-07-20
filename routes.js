b6e9cc7f54db30e01ac91329907d1ff3951b6f25
sethvincent <sethvincent@gmail.com>
2015-07-20 16:22:20 -0700

    change auth:login to auth/:login

--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ module.exports = function (handler, options) {
   router.on(prefix + '/accounts', handler.index.bind(handler))
   router.on(prefix + '/accounts/:key', handler.item.bind(handler))
   router.on(prefix + '/auth', handler.auth.bind(handler))
-  router.on(prefix + '/auth:login', handler.authItem.bind(handler))
-  router.on(prefix + '/auth:logout', handler.authItem.bind(handler))
+  router.on(prefix + '/auth/:login', handler.authItem.bind(handler))
+  router.on(prefix + '/auth/:logout', handler.authItem.bind(handler))
 
   return router
 }
